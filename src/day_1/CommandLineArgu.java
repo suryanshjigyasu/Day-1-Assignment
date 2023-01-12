cb44522e44a67ff596e24f3820ad597589e41459
suryanshjigyasu@gmail.com <suryanshjigyasu@gmail.com>
2023-01-12 22:03:27 +0530

    First day assignment

---
+++ src/day_1/CommandLineArgu.java
@@ -0,0 +1,12 @@
+package day_1;
+
+public class CommandLineArgu 
+{
+	public static void main(String[]args)
+	{
+		String firstName = args[0];
+	System.out.println("first argument is: "+firstName);  
+	
+	}  
+
+}
---
+++ src/day_1/PrintMessage.java
@@ -0,0 +1,10 @@
+package day_1;
+
+public class PrintMessage 
+{
+	public static void main(String[]args)
+	{
+		System.out.println("all are print which are in double quote");
+	}
+
+}
